071f27e70030d429d2796270df0ca30a85a2b8b6
Thundros <32390102+Thundros@users.noreply.github.com>
2019-12-23 09:18:43 -0600

    update...

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -3,7 +3,7 @@
 
 	({
 
-		Extends : Phaser.Scene,
+		Extends : Phaser.Scene, 
 
 		initialize :
 
@@ -11,8 +11,8 @@
 
 			Phaser.Scene.call ( this, {
 
-				key : 'TitleScene',
-				active : false,
+				key : 'TitleScene', 
+				active : false, 
 
 			} );
 
@@ -30,32 +30,32 @@
 
 			this.__buttonAlphaDuration = [
 
-				1000, 1000, 1000,
+				1000, 1000, 1000, 
 
 			];
 
 			this.tweens.add ({
-				targets : this.gameButton,
-				alpha : this.__buttonAlphaLevel [ 0 ],
-				duration : this.__buttonAlphaDuration [ 0 ],
+				targets : this.gameButton, 
+				alpha : this.__buttonAlphaLevel [ 0 ], 
+				duration : this.__buttonAlphaDuration [ 0 ], 
 			});
 
 			this.tweens.add ({
-				targets : this.optionsButton,
-				alpha : this.__buttonAlphaLevel [ 1 ],
-				duration : this.__buttonAlphaDuration [ 1 ],
+				targets : this.optionsButton, 
+				alpha : this.__buttonAlphaLevel [ 1 ], 
+				duration : this.__buttonAlphaDuration [ 1 ], 
 			});
 
 			this.tweens.add ({
-				targets : this.creditsButton,
-				alpha : this.__buttonAlphaLevel [ 2 ],
-				duration : this.__buttonAlphaDuration [ 2 ],
+				targets : this.creditsButton, 
+				alpha : this.__buttonAlphaLevel [ 2 ], 
+				duration : this.__buttonAlphaDuration [ 2 ], 
 			});
 
 			return {
 
-				__buttonAlphaLevel : this.__buttonAlphaLevel,
-				__buttonAlphaDuration : this.__buttonAlphaDuration,
+				__buttonAlphaLevel : this.__buttonAlphaLevel, 
+				__buttonAlphaDuration : this.__buttonAlphaDuration, 
 
 			}
 
@@ -65,38 +65,38 @@
 
 			this.__buttonAlphaLevel = [
 
-				0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 
 
 			];
 
 			this.__buttonAlphaDuration = [
 
-				1000, 1000, 1000,
+				1000, 1000, 1000, 
 
 			];
 
 			this.tweens.add ({
-				targets : this.gameButton,
-				alpha : this.__buttonAlphaLevel [ 0 ],
-				duration : this.__buttonAlphaDuration [ 0 ],
+				targets : this.gameButton, 
+				alpha : this.__buttonAlphaLevel [ 0 ], 
+				duration : this.__buttonAlphaDuration [ 0 ], 
 			});
 
 			this.tweens.add ({
-				targets : this.optionsButton,
-				alpha : this.__buttonAlphaLevel [ 1 ],
-				duration : this.__buttonAlphaDuration [ 1 ],
+				targets : this.optionsButton, 
+				alpha : this.__buttonAlphaLevel [ 1 ], 
+				duration : this.__buttonAlphaDuration [ 1 ], 
 			});
 
 			this.tweens.add ({
-				targets : this.creditsButton,
-				alpha : this.__buttonAlphaLevel [ 2 ],
-				duration : this.__buttonAlphaDuration [ 2 ],
+				targets : this.creditsButton, 
+				alpha : this.__buttonAlphaLevel [ 2 ], 
+				duration : this.__buttonAlphaDuration [ 2 ], 
 			});
 
 			return {
 
-				__buttonAlphaLevel : this.__buttonAlphaLevel,
-				__buttonAlphaDuration : this.__buttonAlphaDuration,
+				__buttonAlphaLevel : this.__buttonAlphaLevel, 
+				__buttonAlphaDuration : this.__buttonAlphaDuration, 
 
 			}
 
@@ -110,26 +110,26 @@
 
 			this.__buttonObj = {
 
-				scene : this.__objData.scene,
-				x : this.__objData.x,
-				y : this.__objData.y,
-				key1 : this.__objData.key1,
-				key2 : this.__objData.key2,
-				text : this.__objData.text,
-				targetScene : this.__objData.targetScene,
-				locked : this.__objData.locked,
+				scene : this.__objData.scene, 
+				x : this.__objData.x, 
+				y : this.__objData.y, 
+				key1 : this.__objData.key1, 
+				key2 : this.__objData.key2, 
+				text : this.__objData.text, 
+				targetScene : this.__objData.targetScene, 
+				locked : this.__objData.locked, 
 
 			}
 
 			this.__buttons = new Button ( this.__buttonObj.scene, {
 
-				x : this.__buttonObj.x,
-				y : this.__buttonObj.y,
-				key1 : this.__buttonObj.key1,
-				key2 : this.__buttonObj.key2,
-				text : this.__buttonObj.text,
-				targetScene : this.__buttonObj.targetScene,
-				locked : this.__buttonObj.locked,
+				x : this.__buttonObj.x, 
+				y : this.__buttonObj.y, 
+				key1 : this.__buttonObj.key1, 
+				key2 : this.__buttonObj.key2, 
+				text : this.__buttonObj.text, 
+				targetScene : this.__buttonObj.targetScene, 
+				locked : this.__buttonObj.locked, 
 
 			} );
 
@@ -187,41 +187,41 @@
 
 			this.__buttonX = [
 
-				( __config.width / 2 ),
-				( __config.width / 2 ),
-				( __config.width / 2 ),
+				( __config.width / 2 ), 
+				( __config.width / 2 ), 
+				( __config.width / 2 ), 
 
 			];
 
 			this.__buttonY = [
 
-				( ( __config.height / 2 ) - 100 ),
-				( ( __config.height / 2 ) ),
+				( ( __config.height / 2 ) - 100 ), 
+				( ( __config.height / 2 ) ), 
 				( ( __config.height / 2 ) + 100 )
 
 			];
 
 			this.__buttonKeys = [
 
-				'blueButton1', 'blueButton2',
+				'blueButton1', 'blueButton2', 
 
 			];
 
 			this.__buttonText = [
 
-				'Play', 'Options', 'Credits',
+				'Play', 'Options', 'Credits', 
 
 			];
 
 			this.__buttonTargetScene = [
 
-				'GameScene', 'OptionsScene', 'CreditsScene',
+				'GameScene', 'OptionsScene', 'CreditsScene', 
 
 			];
 
 			this.__buttonLocked = [
 
-				true, true, true,
+				true, true, true, 
 
 			];
 
@@ -232,8 +232,8 @@
 				// Game
 
 				this.__button [ this.__i ] = this.CreateGameButton ({
-					scene : this.__scene, x : this.__buttonX [ this.__i ], y : this.__buttonY [ this.__i ],
-					key1 : this.__buttonKeys [ 0 ], key2 : this.__buttonKeys [ 1 ], text : this.__buttonText [ this.__i ],
+					scene : this.__scene, x : this.__buttonX [ this.__i ], y : this.__buttonY [ this.__i ], 
+					key1 : this.__buttonKeys [ 0 ], key2 : this.__buttonKeys [ 1 ], text : this.__buttonText [ this.__i ], 
 					targetScene : this.__buttonTargetScene [ this.__i ], locked : this.__buttonLocked [ this.__i ]
 				});
 
@@ -243,6 +243,9 @@
 
 				this.__buttonAlphaLevel [ this.__i ] = this.__fadeInMenu ( ).__buttonAlphaLevel [ this.__i ];
 				this.__buttonAlphaDuration [ this.__i ] = this.__fadeInMenu ( ).__buttonAlphaDuration [ this.__i ];
+
+				console.log ( this.__buttonAlphaLevel [ this.__i ] );
+				console.log ( this.__buttonAlphaDuration [ this.__i ] );
 
 				/*
 
@@ -299,27 +302,27 @@
 
 			this.__soundTrack = [
 
-				'bgMusic', 'lvl1Music',
+				'bgMusic', 'lvl1Music', 
 
 			];
 
 			this.__soundData = [
 
 				{
-					volume : 0.1,
-					loop : true,
-				},
+					volume : 0.1, 
+					loop : true, 
+				}, 
 
 				{
-					volume : 0.1,
-					loop : true,
-				},
+					volume : 0.1, 
+					loop : true, 
+				}, 
 
 			];
 
 			this.createAudio({
-				soundID : this.__soundTrack [ 0 ],
-				soundData : this.__soundData [ 0 ],
+				soundID : this.__soundTrack [ 0 ], 
+				soundData : this.__soundData [ 0 ], 
 			});
 
 		}, 
@@ -328,7 +331,7 @@
 
 			Phaser.Display.Align.In.Center ( gameObject, this.add.zone (
 
-				( __config.width / 2 ), ( ( __config.height / 2 ) - ( offset * 100 ) ),
+				( __config.width / 2 ), ( ( __config.height / 2 ) - ( offset * 100 ) ), 
 				__config.width, __config.height
 
 			) );
@@ -353,8 +356,8 @@
 
 				this.__button [ this.__i ].update (
 
-					this.__button [ this.__i ].alpha,
-					this.__buttonAlphaLevel [ this.__i ],
+					this.__button [ this.__i ].alpha, 
+					this.__buttonAlphaLevel [ this.__i ], 
 					this.__buttonAlphaDuration [ this.__i ]
 
 				);
