bd85779d30f8ce17d55bf6c140b76d17ec2a1e54
Thundros <32390102+Thundros@users.noreply.github.com>
2020-01-10 13:56:23 -0600

    update...

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -206,28 +206,30 @@
 				this.__buttonFadeInAlphaLevel [ this.__i ] = this.__fadeInMenu ( ).__buttonAlphaLevel [ this.__i ];
 				this.__buttonFadeInAlphaDuration [ this.__i ] = this.__fadeInMenu ( ).__buttonAlphaDuration [ this.__i ];
 
+				/*
+
 				this.tweens.add ({
 					targets : this.__button [ this.__i ], 
 					alpha : this.__buttonFadeInAlphaLevel [ this.__i ], 
 					duration : this.__buttonFadeInAlphaDuration [ this.__i ], 
 				});
+				
+				*/
 
 				this.__btnObjects.push ( this.__button [ this.__i ] );
 				console.log ( this.__button [ this.__i ] );
 
 				this.__btnObjects [ this.__i ].button.on ( 'pointerdown', function ( ) {
 					this.__scene.tweens.add ({
-						targets : [
-							this.__btnObjects, this.__buttonText [ this.__i ], 
-						], 
-						targetScenes : this.__buttonTargetScene [ this.__i ], 
+						targets : this.__btnObjects [ this.__i ], 
 						repeat : 0, 
 						duration : 750, 
 						alpha : { from : 1.0, to : 0.0 }, 
-						easeType : 'Linear', 
 						yoyo : false, 
 						onComplete : ( ) => {
-							this.__scene.scene.start ( this.__buttonTargetScene );
+							this.__scene.scene.start (
+								this.__buttonTargetScene [ this.__i ]
+							);
 						}
 					});
 				}.bind ( this ) );
